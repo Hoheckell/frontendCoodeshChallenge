25577463917712885281093b7d9e19000bee7f95
Hoheckell <hoheckel.filho@prideglobal.com>
2023-10-27 10:49:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';  
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://placehold.co/400" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code>
+        </p> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
